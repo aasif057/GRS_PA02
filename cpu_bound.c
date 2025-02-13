18d8d2e229f1380952400b4c193e49b9a399a85b
aasif057 <you@mdaasif885@gmail.com>
2025-02-13 23:02:14 +0530

    Updated

--- cpu_bound.c
+++ cpu_bound.c
@@ -6,9 +6,9 @@
 
 #define UPPER_RANGE 500000  // Upper limit for prime checking
 
-int prime_count = 0; // Shared counter for prime numbers
+int count = 0; // Shared counter for prime numbers
 pthread_mutex_t lock;
-int NUM_THREADS; // Number of threads
+int thread_count; // Number of threads
 
 // Thread structure
 typedef struct {
@@ -16,10 +16,12 @@ typedef struct {
 } ThreadData;
 
 // Function to check if a number is prime
-int check_prime(int num) {
-    if (num < 2) return 0;
-    for (int i = 2; i <= sqrt(num); i++) {
-        if (num % i == 0) return 0;
+int check_prime(int n) {
+    if (n < 2) 
+        return 0;
+    for (int i = 2; i <= sqrt(n); i++) {
+        if (n % i == 0) 
+            return 0;
     }
     return 1;
 }
@@ -33,9 +35,9 @@ void *count_primes(void *arg) {
             local_count++;
         }
     }
-    //Acquire lock before updating the global prime_count
+    //Acquire lock before updating the global count
     pthread_mutex_lock(&lock);
-    prime_count += local_count;
+    count += local_count;
     pthread_mutex_unlock(&lock);
     pthread_exit(NULL);
 }
@@ -43,40 +45,40 @@ void *count_primes(void *arg) {
 int main(int argc, char *argv[]) {
     // Error check if number of threads is not passed as an argument
     if (argc != 2) {
-        printf("Usage: %s <num_threads>\n", argv[0]);
+        printf("Usage: %s <thread_count>\n", argv[0]);
         return 1;
     }
     // Error check if number of threads is invalid
-    NUM_THREADS = atoi(argv[1]);
-    if (NUM_THREADS <= 0) {
+    thread_count = atoi(argv[1]);
+    if (thread_count <= 0) {
         printf("Invalid number of threads.\n");
         return 1;
     }
     
-    pthread_t threads[NUM_THREADS]; //create threads
-    ThreadData thread_data[NUM_THREADS]; 
-    int range = UPPER_RANGE / NUM_THREADS;
+    pthread_t threads[thread_count]; //create threads
+    ThreadData thread_data[thread_count]; 
+    int range = UPPER_RANGE / thread_count;
     pthread_mutex_init(&lock, NULL);
     
     struct timespec start, end;
     clock_gettime(CLOCK_MONOTONIC, &start);
     
     // Create threads
-    for (int i = 0; i < NUM_THREADS; i++) {
+    for (int i = 0; i < thread_count; i++) {
         thread_data[i].start = i * range;
-        thread_data[i].end = (i == NUM_THREADS - 1) ? UPPER_RANGE : (i + 1) * range;
+        thread_data[i].end = (i == thread_count - 1) ? UPPER_RANGE : (i + 1) * range;
         pthread_create(&threads[i], NULL, count_primes, &thread_data[i]);
     }
     
     // Join threads
-    for (int i = 0; i < NUM_THREADS; i++) {
+    for (int i = 0; i < thread_count; i++) {
         pthread_join(threads[i], NULL);
     }
     
     clock_gettime(CLOCK_MONOTONIC, &end);
     double elapsed = (end.tv_sec - start.tv_sec) + (end.tv_nsec - start.tv_nsec) / 1e9;
     
-    printf("Total prime numbers: %d\n", prime_count);
+    printf("Total prime numbers: %d\n", count);
     printf("Execution Time: %f seconds\n", elapsed);
     
     pthread_mutex_destroy(&lock);
