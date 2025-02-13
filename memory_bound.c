7ec341ea3c0d37c576df4a5930414834ce4332e0
aasif057 <you@mdaasif885@gmail.com>
2025-02-13 22:58:59 +0530

    Updated

--- memory_bound.c
+++ memory_bound.c
@@ -3,24 +3,24 @@
 #include <pthread.h>
 #include <time.h>
 
-#define ARRAY_SIZE 100000000  // Large array size
+#define ARR_SIZE 100000000  // Large arr size
 
-int *array;
+int *arr;
 long long sum = 0; //global var to save sum
 pthread_mutex_t lock;
-int NUM_THREADS; // Dynamic number of threads
+int thread_count; // Dynamic number of threads
 
 // Thread structure
 typedef struct {
-    int start_idx, end_idx;
+    int start, end;
 } ThreadData;
 
 //function to calculate sum
 void *get_sum(void *arg) {
     ThreadData *data = (ThreadData *)arg;
     long long local_sum = 0; // var to save the local sum on each thread
-    for (int i = data->start_idx; i < data->end_idx; i++) {
-        local_sum += array[i];
+    for (int i = data->start; i < data->end; i++) {
+        local_sum += arr[i];
     }
     pthread_mutex_lock(&lock); //locking global var sum to avoid race-around cond
     sum += local_sum;
@@ -30,40 +30,40 @@ void *get_sum(void *arg) {
 
 int main(int argc, char *argv[]) {
     if (argc != 2) {
-        printf("Usage: %s <num_threads>\n", argv[0]);
+        printf("Usage: %s <thread_count>\n", argv[0]);
         return 1;
     }
     
-    NUM_THREADS = atoi(argv[1]);
-    if (NUM_THREADS <= 0) {
+    thread_count = atoi(argv[1]);
+    if (thread_count <= 0) {
         printf("Invalid number of threads.\n");
         return 1;
     }
     
-    // Allocate and initialize array
-    array = (int *)malloc(ARRAY_SIZE * sizeof(int));
+    // Allocate and initialize arr
+    arr = (int *)malloc(ARR_SIZE * sizeof(int));
     srand(time(NULL));
-    for (int i = 0; i < ARRAY_SIZE; i++) {
-        array[i] = rand() % 100;
+    for (int i = 0; i < ARR_SIZE; i++) {
+        arr[i] = rand() % 100;
     }
     
-    pthread_t threads[NUM_THREADS];
-    ThreadData thread_data[NUM_THREADS];
-    int chunk_size = ARRAY_SIZE / NUM_THREADS;
+    pthread_t threads[thread_count];
+    ThreadData thread_data[thread_count];
+    int chunk_size = ARR_SIZE / thread_count;
     pthread_mutex_init(&lock, NULL);
     
     struct timespec start, end;
     clock_gettime(CLOCK_MONOTONIC, &start);
     
     // Create threads
-    for (int i = 0; i < NUM_THREADS; i++) {
-        thread_data[i] = (ThreadData){.start_idx = i * chunk_size, .end_idx = (i + 1) * chunk_size};
-        if (i == NUM_THREADS - 1) thread_data[i].end_idx = ARRAY_SIZE;
+    for (int i = 0; i < thread_count; i++) {
+        thread_data[i] = (ThreadData){.start = i * chunk_size, .end = (i + 1) * chunk_size};
+        if (i == thread_count - 1) thread_data[i].end = ARR_SIZE;
         pthread_create(&threads[i], NULL, get_sum, &thread_data[i]);
     }
     
     // Join threads
-    for (int i = 0; i < NUM_THREADS; i++) {
+    for (int i = 0; i < thread_count; i++) {
         pthread_join(threads[i], NULL);
     }
     
@@ -74,6 +74,6 @@ int main(int argc, char *argv[]) {
     printf("Execution Time: %f seconds\n", elapsed);
     
     pthread_mutex_destroy(&lock);
-    free(array);
+    free(arr);
     return 0;
 }
