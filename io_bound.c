f676fee269b3002bae2f667d02eb83c25ccf82a0
aasif057 <you@mdaasif885@gmail.com>
2025-02-13 23:01:02 +0530

    Updated

--- io_bound.c
+++ io_bound.c
@@ -4,89 +4,90 @@
 #include <time.h>
 
 #define FILE_NAME "test_file.txt"
-#define NUM_LINES 100000  // lines per thread
+#define LINES_COUNT 10000  // Total lines to write
 
-int NUM_THREADS; // number of threads
+int thread_count;
 pthread_mutex_t lock;
 
-// function for read and write on a single file
-void *io_task(void *arg) {
+// Write Task
+void *write_func(void *arg) {
     int thread_id = *(int *)arg;
-    
-    // lock the file so that only one thread access the file at a time for write operation
+
     pthread_mutex_lock(&lock);
-    
-    // Write to the shared file
     FILE *fp = fopen(FILE_NAME, "a");
     if (!fp) {
         perror("File open error");
         pthread_mutex_unlock(&lock);
         pthread_exit(NULL);
     }
-    for (int i = 0; i < NUM_LINES; i++) {
+    for (int i = 0; i < (LINES_COUNT / thread_count); i++) {
         fprintf(fp, "Thread %d - Line %d\n", thread_id, i);
     }
     fclose(fp);
-
-    //unlock the file
     pthread_mutex_unlock(&lock);
 
-    // lock the file so that only one thread access the file at a time for reading
-    pthread_mutex_lock(&lock);
-    
-    // Read from shared file
-    fp = fopen(FILE_NAME, "r");
+    pthread_exit(NULL);
+}
+
+// Read Task 
+void *read_func(void *arg) {
+    int thread_id = *(int *)arg;
+
+    FILE *fp = fopen(FILE_NAME, "r");
     if (!fp) {
         perror("File read error");
-        pthread_mutex_unlock(&lock);
         pthread_exit(NULL);
     }
     char buffer[100];
     while (fgets(buffer, sizeof(buffer), fp)) {
-        //Simulating read operation
+        // Simulating read operation
     }
     fclose(fp);
-    
-    pthread_mutex_unlock(&lock);
-    
+
     pthread_exit(NULL);
 }
 
 int main(int argc, char *argv[]) {
     if (argc != 2) {
-        printf("Usage: %s <num_threads>\n", argv[0]);
+        printf("Usage: %s <thread_count>\n", argv[0]);
         return 1;
     }
-    
-    NUM_THREADS = atoi(argv[1]);
-    if (NUM_THREADS <= 0) {
+
+    thread_count = atoi(argv[1]);
+    if (thread_count <= 0) {
         printf("Invalid number of threads.\n");
         return 1;
     }
-    
-    pthread_t threads[NUM_THREADS];
-    int thread_ids[NUM_THREADS];
+
+    pthread_t threads[thread_count];
+    int thread_ids[thread_count];
     pthread_mutex_init(&lock, NULL);
-    
+
     struct timespec start, end;
     clock_gettime(CLOCK_MONOTONIC, &start);
-    
-    // Create threads
-    for (int i = 0; i < NUM_THREADS; i++) {
+
+    // Writing Phase (Parallel)
+    for (int i = 0; i < thread_count; i++) {
         thread_ids[i] = i;
-        pthread_create(&threads[i], NULL, io_task, &thread_ids[i]);
+        pthread_create(&threads[i], NULL, write_func, &thread_ids[i]);
     }
-    
-    // Join threads
-    for (int i = 0; i < NUM_THREADS; i++) {
+    for (int i = 0; i < thread_count; i++) {
         pthread_join(threads[i], NULL);
     }
-    
+
+    // Reading Phase (Parallel)
+    for (int i = 0; i < thread_count; i++) {
+        pthread_create(&threads[i], NULL, read_func, &thread_ids[i]);
+    }
+    for (int i = 0; i < thread_count; i++) {
+        pthread_join(threads[i], NULL);
+    }
+
     clock_gettime(CLOCK_MONOTONIC, &end);
     double elapsed = (end.tv_sec - start.tv_sec) + (end.tv_nsec - start.tv_nsec) / 1e9;
-    
+
     printf("Execution Time: %f seconds\n", elapsed);
-    
+
     pthread_mutex_destroy(&lock);
     return 0;
 }
